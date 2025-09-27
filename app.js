ecc4b45f1d30e44c12e1db8c498e4da873f64f4a
Elian Arismendi <naranjax@MAC-68553.local>
2025-09-27 16:57:28 -0300

    feat: finish binary parse project

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PHRASES } from "./constants.js";
 const parseForm = document.querySelector("#parse-form");
 const submitButton = document.querySelector("#submit-button");
 const parsedMessageDiv = document.querySelector("#parsed-message");
+const messageInputLabel = document.querySelector("#message-input");
 
 const convertTextareaInput = document.querySelector("#convert-input");
 const radioBinary = document.querySelector("#input-type-binary");
@@ -15,15 +16,40 @@ const getRandomValue = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
+const generateAnimatedLi = (words, delay = null) => {
+  const li = document.createElement("li");
+  const animationDelay = delay ?? getRandomValue(3, 15);
+
+  li.setAttribute(
+    "style",
+    `left: ${getRandomValue(1, 100)}%; 
+      animation-delay: ${animationDelay}s;   
+      animation-duration: ${getRandomValue(10, 20)}s;`
+  );
+
+  for (let i = 0; i < words.length; i++) {
+    const char = words[i];
+
+    const span = document.createElement("span");
+    span.textContent = char;
+
+    li.appendChild(span);
+  }
+
+  return li;
+};
+
 const validateForm = (e) => {
   const val = e.target.value.trim();
+
+  messageInputLabel.textContent = val === "binary" ? "Put your binary code here!" : "Put your message here!";
 
   if (val?.length) {
     if (validationsPassedCounter < 2) validationsPassedCounter++;
   } else {
     if (validationsPassedCounter > 0) validationsPassedCounter--;
   }
-  console.log(validationsPassedCounter);
+
   submitButton.disabled = validationsPassedCounter !== 2;
 };
 
@@ -33,12 +59,11 @@ const parseMessage = (message, conversionMethod) => {
     for (let i = 0; i < message.length; i++) {
       const unicode = message.charCodeAt(i);
       let parsedChar = unicode.toString("2");
-      console.log(parsedChar);
-      console.log(parsedChar.length);
+
       if (parsedChar.length < 8) {
         parsedChar = parsedChar.padStart(8, "0");
       }
-      console.log("parsed: " + parsedChar);
+
       parsedMessage += parsedChar + " ";
     }
     return parsedMessage;
@@ -47,17 +72,21 @@ const parseMessage = (message, conversionMethod) => {
     const binaryArr = [];
     let binaryArrElementSize = 8;
     let currentBinaryArrIndex = 0;
+
     for (let i = 0; i < binaryString.length; i++) {
       binaryArr[currentBinaryArrIndex] = binaryArr[currentBinaryArrIndex] ?? "";
       binaryArr[currentBinaryArrIndex] += binaryString[i];
       binaryArrElementSize--;
+
       if (binaryArrElementSize === 0) {
         binaryArrElementSize = 8;
         currentBinaryArrIndex++;
       }
     }
+
     const uintArray = new Uint8Array(binaryArr.map((el) => parseInt(el, 2)));
     const parsedMessage = Array.from(uintArray, (el) => String.fromCharCode(el)).join("");
+
     return parsedMessage;
   }
 };
@@ -79,25 +108,25 @@ parseForm.addEventListener("submit", (e) => {
 document.addEventListener("DOMContentLoaded", () => {
   const wordListContainer = document.querySelector(".word-list");
 
-  PHRASES.forEach((words) => {
-    const li = document.createElement("li");
-    li.setAttribute(
-      "style",
-      `left: ${getRandomValue(1, 100)}%; animation-delay: ${getRandomValue(
-        3,
-        15
-      )}s;   animation-duration: ${getRandomValue(10, 20)}s;`
-    );
+  let phrases = [];
 
-    for (let i = 0; i < words.length; i++) {
-      const char = words[i];
+  if (window.innerWidth < 480) {
+    // for smarthphones
+    phrases = PHRASES.slice(0, 30);
+  } else if (window.innerWidth < 1024) {
+    phrases = PHRASES.slice(0, 70);
+  } else {
+    phrases = PHRASES;
+  }
 
-      const span = document.createElement("span");
-      span.textContent = char;
+  phrases.forEach((words) => {
+    const rdm = getRandomValue(-1, 2);
+    const li = generateAnimatedLi(words, rdm);
+    wordListContainer.appendChild(li);
+  });
 
-      li.appendChild(span);
-    }
-
+  phrases.forEach((words) => {
+    const li = generateAnimatedLi(words);
     wordListContainer.appendChild(li);
   });
 });
